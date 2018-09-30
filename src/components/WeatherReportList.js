e34c357d3c6d697cb1d25ecff64576e34f99b90d
ashlinaronin <ashlinaronin@gmail.com>
2018-09-30 16:00:20 -0700

    Move logic out of WeatherReportListItem to make it more testable

--- src/components/WeatherReportList.js
+++ src/components/WeatherReportList.js
@@ -2,18 +2,26 @@ import React from "react";
 import PropTypes from "prop-types";
 import WeatherReportListItem from "./WeatherReportListItem";
 import { weatherType } from "../types";
+import { THRESHOLD_HEATING, THRESHOLD_AC } from "../constants";
 import "./WeatherReportList.css";
 
 const WeatherReportList = ({ weatherDataArray }) => {
   return (
     <ul className="weather-report-list">
-      {weatherDataArray.map((weatherData, index) => (
-        <WeatherReportListItem
-          weatherData={weatherData}
-          date={index + 1}
-          key={weatherData.daily.data[0].time}
-        />
-      ))}
+      {weatherDataArray.map((weatherData, index) => {
+        const dailyData = weatherData.daily.data[0];
+
+        return (
+          <WeatherReportListItem
+            acTurnedOn={dailyData.temperatureMax > THRESHOLD_AC}
+            heatingTurnedOn={dailyData.temperatureMin < THRESHOLD_HEATING}
+            minDailyTemp={dailyData.temperatureMin}
+            maxDailyTemp={dailyData.temperatureMax}
+            date={index + 1}
+            key={weatherData.daily.data[0].time}
+          />
+        );
+      })}
     </ul>
   );
 };
--- src/components/WeatherReportListItem.js
+++ src/components/WeatherReportListItem.js
@@ -1,16 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { weatherType } from "../types";
 import "./WeatherReportListItem.css";
-import { THRESHOLD_AC, THRESHOLD_HEATING } from "../constants";
 
 const BASE_CLASS = "weather-report-list__item";
 
-const WeatherReportListItem = ({ weatherData, date }) => {
-  const dailyData = weatherData.daily.data[0];
-  const acTurnedOn = dailyData.temperatureMax > THRESHOLD_AC;
-  const heatingTurnedOn = dailyData.temperatureMin < THRESHOLD_HEATING;
-
+const WeatherReportListItem = ({
+  acTurnedOn,
+  heatingTurnedOn,
+  maxDailyTemp,
+  minDailyTemp,
+  date
+}) => {
   const classes = getClasses(acTurnedOn, heatingTurnedOn);
 
   return (
@@ -18,10 +18,10 @@ const WeatherReportListItem = ({ weatherData, date }) => {
       <strong>{date}.</strong>
       &nbsp;
       <br />
-      {dailyData.temperatureMax}
+      {maxDailyTemp}
       &deg;
       <br />
-      {dailyData.temperatureMin}
+      {minDailyTemp}
       &deg;
     </li>
   );
@@ -42,7 +42,10 @@ function getClasses(acTurnedOn, heatingTurnedOn) {
 }
 
 WeatherReportListItem.propTypes = {
-  weatherData: weatherType,
+  acTurnedOn: PropTypes.bool.isRequired,
+  heatingTurnedOn: PropTypes.bool.isRequired,
+  maxDailyTemp: PropTypes.number.isRequired,
+  minDailyTemp: PropTypes.number.isRequired,
   date: PropTypes.number.isRequired
 };
 
