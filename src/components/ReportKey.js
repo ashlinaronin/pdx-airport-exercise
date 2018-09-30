fd87cdacce72d4a806a97f7d81a76dfb23355bf9
ashlinaronin <ashlinaronin@gmail.com>
2018-09-30 15:04:16 -0700

    Move report key to its own component

---
+++ src/components/ReportKey.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./ReportKey.css";
+
+const ReportKey = () => (
+  <div className="report-key">
+    <div>
+      <strong>Key:</strong>
+    </div>
+    <div className="report-key__type">
+      <div className="report-key__block report-key__block--heat">
+        <p>Heat</p>
+      </div>
+      <div className="report-key__text">Heating on at least once</div>
+    </div>
+    <div className="report-key__type">
+      <div className="report-key__block report-key__block--ac">
+        <p>AC</p>
+      </div>
+      <div className="report-key__text">AC on at least once</div>
+    </div>
+    <div className="report-key__type">
+      <div className="report-key__block report-key__block--both">
+        <p>Both</p>
+      </div>
+      <div className="report-key__text">
+        Both heating and AC on at least once
+      </div>
+    </div>
+  </div>
+);
+
+export default ReportKey;
--- src/components/WeatherReportSummary.js
+++ src/components/WeatherReportSummary.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { weatherType } from "../types";
 import { THRESHOLD_HEATING, THRESHOLD_AC } from "../constants";
+import ReportKey from "./ReportKey";
 import "./WeatherReportSummary.css";
 
 const WeatherReportSummary = ({ weatherDataArray, month, year }) => {
@@ -21,28 +22,7 @@ const WeatherReportSummary = ({ weatherDataArray, month, year }) => {
 
   return (
     <div className="weather-report-summary">
-      <div className="report-key">
-        <div>Key:</div>
-        <div>
-          <div className="report-key__block report-key__block--heat">
-            <p>Heat</p>
-          </div>
-          <div>Heating on at least once</div>
-        </div>
-
-        <div>
-          <div className="report-key__block report-key__block--ac">
-            <p>AC</p>
-          </div>
-          <div>AC on at least once</div>
-        </div>
-        <div>
-          <div className="report-key__block report-key__block--both">
-            <p>Both</p>
-          </div>
-          <div>Both heating and AC on at least once</div>
-        </div>
-      </div>
+      <ReportKey />
       <p>
         During {month} {year}, the AC system at Portland International Airport
         was turned on at least once a day for {totalAc} days. The heating system
