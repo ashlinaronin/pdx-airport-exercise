ee30ad3e9edfabb5f74d3437493b167488a93d45
ashlinaronin <ashlinaronin@gmail.com>
2018-09-30 13:27:53 -0700

    Make sure CORS middleware is engaged before caching so that cached responses have correct CORS headers

--- backend/api.js
+++ backend/api.js
@@ -1,19 +1,26 @@
 const Koa = require("koa");
 const cors = require("koa-cors");
-const cash = require('koa-cash');
-const convert = require('koa-convert');
-const LRU = require('lru-cache');
+const cash = require("koa-cash");
+const convert = require("koa-convert");
+const LRU = require("lru-cache");
 const logger = require("koa-logger");
 const app = new Koa();
 const weatherRoutes = require("./routes/weather");
 const router = require("koa-router")();
 const cache = LRU(1000);
 
+app.use(logger());
+app.use(cors());
+
 // Cache responses to avoid unnecessary calls to external API
-app.use(convert(cash({
-  get: key => cache.get(key),
-  set: (key, value) => cache.set(key, value)
-})));
+app.use(
+  convert(
+    cash({
+      get: key => cache.get(key),
+      set: (key, value) => cache.set(key, value)
+    })
+  )
+);
 
 app.use(async (ctx, next) => {
   const cashed = await ctx.cashed();
@@ -21,11 +28,8 @@ app.use(async (ctx, next) => {
   await next();
 });
 
-app
-  .use(logger())
-  .use(cors())
-  .use(router.allowedMethods())
-  .use(router.routes());
+app.use(router.allowedMethods());
+app.use(router.routes());
 
 router.use("/weather", weatherRoutes.routes());
 
