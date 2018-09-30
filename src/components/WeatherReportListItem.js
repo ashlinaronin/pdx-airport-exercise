ecfdefb8e5616531bd8b238d783696316b1ac213
ashlinaronin <ashlinaronin@gmail.com>
2018-09-30 10:32:52 -0700

    Calculate weather AC and/or heating were turned on and show in report

--- src/components/WeatherReportListItem.js
+++ src/components/WeatherReportListItem.js
@@ -2,9 +2,13 @@ import React from "react";
 import {weatherType} from "../types";
 
 const WeatherReportListItem = ({ weatherData }) => {
+  const dailyData = weatherData.daily.data[0];
+  const acTurnedOn = dailyData.temperatureMax > 75;
+  const heatingTurnedOn = dailyData.temperatureMin < 62;
+
   return (
     <li>
-      {JSON.stringify(weatherData)}
+      AC={acTurnedOn ? 'yes' : 'no'}, Heating={heatingTurnedOn ? 'yes' : 'no'}
     </li>
   );
 };
