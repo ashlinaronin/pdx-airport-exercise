5a8c0a91bcc0abcd95af981f4a38cc72bd983d8b
ashlinaronin <ashlinaronin@gmail.com>
2018-09-30 09:28:10 -0700

    Add API method to get weather for month

--- backend/routes/weather.js
+++ backend/routes/weather.js
@@ -1,13 +1,26 @@
 const router = require("koa-router")();
-const { getWeather } = require("../services/weather");
+const { getWeather, getWeatherForMonth } = require("../services/weather");
 
-router.get("/:latitude/:longitude", async ctx => {
+router.get("/today/:latitude/:longitude", async ctx => {
   try {
     // TODO: verify params present
     ctx.body = await getWeather(ctx.params.latitude, ctx.params.longitude);
   } catch (err) {
     console.error(err);
     ctx.throw(500, "Weather: error fetching weather from external API");
+  }
+});
+
+router.get("/month/:month/:latitude/:longitude", async ctx => {
+  try {
+    ctx.body = await getWeatherForMonth(
+      ctx.params.month,
+      ctx.params.latitude,
+      ctx.params.longitude
+    );
+  } catch (err) {
+    console.erorr(err);
+    ctx.throw(500, "Weather: error fetching month's weather from external API");
   }
 });
 
--- backend/services/weather.js
+++ backend/services/weather.js
@@ -1,13 +1,25 @@
 const fetch = require("isomorphic-fetch");
 const API_BASE_URL = "//api.darksky.net/forecast";
 const API_SECRET = "dd9b259a5201c1f828bdb05bd0c80321";
+const FETCH_OPTIONS = { headers: { "Accept-Encoding": "gzip" } };
 
 async function getWeather(latitude, longitude) {
   const url = `${API_BASE_URL}/${API_SECRET}/${latitude},${longitude}`;
-  const response = await fetch(url);
-  return await response.json();
+  const response = await fetch(url, FETCH_OPTIONS);
+  return response.json();
+}
+
+async function getWeatherForMonth(month, latitude, longitude) {
+  // Convert given month into a Unix timestamp in PST
+  const date = new Date(`1 ${month} 2018 00:00:00 GMT-0800`);
+  const timestamp = Math.round(date.getTime() / 1000);
+
+  const url = `${API_BASE_URL}/${API_SECRET}/${latitude},${longitude},${timestamp}?exclude=currently,hourly,flags`;
+  const response = await fetch(url, FETCH_OPTIONS);
+  return response.json();
 }
 
 module.exports = {
-  getWeather
+  getWeather,
+  getWeatherForMonth
 };
