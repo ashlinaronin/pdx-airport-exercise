47489153e57e2976b564d2d813fcc45bf1481d9b
ashlinaronin <ashlinaronin@gmail.com>
2018-09-30 12:17:08 -0700

    Add max and min temps to list items

--- src/components/WeatherReportListItem.js
+++ src/components/WeatherReportListItem.js
@@ -2,20 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import { weatherType } from "../types";
 import "./WeatherReportListItem.css";
+import { THRESHOLD_AC, THRESHOLD_HEATING } from "../constants";
 
 const BASE_CLASS = "weather-report-list__item";
 
 const WeatherReportListItem = ({ weatherData, date }) => {
   const dailyData = weatherData.daily.data[0];
-  const acTurnedOn = dailyData.temperatureMax > 75;
-  const heatingTurnedOn = dailyData.temperatureMin < 62;
+  const acTurnedOn = dailyData.temperatureMax > THRESHOLD_AC;
+  const heatingTurnedOn = dailyData.temperatureMin < THRESHOLD_HEATING;
 
   const classes = getClasses(acTurnedOn, heatingTurnedOn);
 
   return (
     <li className={classes}>
       Date=
-      {date}, AC=
+      {date}, Maxtemp=
+      {dailyData.temperatureMax}, Mintemp=
+      {dailyData.temperatureMin}, AC=
       {acTurnedOn ? "yes" : "no"}, Heating=
       {heatingTurnedOn ? "yes" : "no"}
     </li>
--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,13 @@
 export const PDX_LATITUDE = 45.5898;
 export const PDX_LONGITUDE = -122.5951;
 
+export const THRESHOLD_AC = 75;
+export const THRESHOLD_HEATING = 62;
+
 export const AVAILABLE_MONTHS = [
-  "JANUARY",
-  "FEBRUARY",
-  "MARCH",
-  "APRIL",
-  "MAY"
+  "January",
+  "February",
+  "March",
+  "April",
+  "May"
 ];
