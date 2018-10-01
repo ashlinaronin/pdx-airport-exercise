7a669214419d1d985a4848cfd0fbe477beaae1c9
ashlinaronin <ashlinaronin@gmail.com>
2018-09-30 18:23:37 -0700

    Swap order of AC and heating in key to match text

--- src/components/ReportKey.js
+++ src/components/ReportKey.js
@@ -7,16 +7,16 @@ const ReportKey = () => (
       <strong>Key:</strong>
     </div>
     <div className="report-key__type">
-      <div className="report-key__block report-key__block--heat">
-        <p>Heat</p>
-      </div>
-      <div className="report-key__text">Heating on at least once</div>
-    </div>
-    <div className="report-key__type">
       <div className="report-key__block report-key__block--ac">
         <p>AC</p>
       </div>
       <div className="report-key__text">AC on at least once</div>
+    </div>
+    <div className="report-key__type">
+      <div className="report-key__block report-key__block--heat">
+        <p>Heat</p>
+      </div>
+      <div className="report-key__text">Heating on at least once</div>
     </div>
     <div className="report-key__type">
       <div className="report-key__block report-key__block--both">
